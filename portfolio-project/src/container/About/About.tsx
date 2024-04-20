7506f5f96d25185e8d42d3f7fd621acae088f983
ihawxg <ihawxg@gmail.com>
2024-04-20 12:08:22 +0300

    sanity backend & cms setup

--- portfolio-project/src/container/About/About.tsx
+++ portfolio-project/src/container/About/About.tsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import React, { ImgHTMLAttributes, useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
-import about1 from '../../assets/about01.png'
-import about2 from '../../assets/about02.png'
-import about3 from '../../assets/about03.png'
-import about4 from '../../assets/about04.png'
-
+import { urlFor, client } from '../../client'
+import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 import './About.scss'
 
-const abouts = [
-  { title: 'Web Development', description: 'I Am a good web developer', imgUrl: about1 },
-  { title: 'Web Design', description: 'I Am a good web developer', imgUrl: about2 },
-  { title: 'UI/UX', description: 'I Am a good web developer', imgUrl: about3 },
-  { title: 'Web Animations', description: 'I Am a good web developer', imgUrl: about4 },
-]
+interface abouts {
+  title: string
+  description: string
+  imgUrl: string | SanityImageSource
+}
 
 export const About = () => {
+
+  const [abouts, setAbouts] = useState<abouts[]>([])
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+    client.fetch(query)
+      .then((data) => {
+        setAbouts(data)
+      })
+  }, []);
+
   return (
     <div>
-      <h2 className='head-text'>I Know That
-        <span>Good Design</span>
-        <br />
-        means
-        <span>Good Business</span>
-      </h2>
+      <h2 className='head-text'>I Know That <span>Good Design</span> <br /> means <span>Good Business</span></h2>
       <div className='app__profiles'>
         {abouts.map((about, index) =>
           <motion.div
@@ -32,7 +34,7 @@ export const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl).url()} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
